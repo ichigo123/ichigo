d0106acdb5f03d989dc29d825e97f4d1f05b0fa5
ichigo123 <354066037@qq.com>
2017-03-14 15:55:19 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,95 +1,63 @@
 <?php
-/*
-   test
-*/
-traceHttp();
+namespace Home\Controller;
+use Think\Controller;
+class IndexController extends Controller {
+    
+    public function index(){
+       
+}
+  public function check()
+{  
+                
+                /**
+                  * wechat php test
+                  */
+                
+                //define your token
+                define("TOKEN", "weixin");
+      $this->valid();
 
-define("TOKEN", "weixin");
-$wechatObj = new wechatCallbackapiTest();
-if (isset($_GET['echostr'])) {
-    $wechatObj->valid();
-}else{
-    $wechatObj->responseMsg();
+}
+    
+                 public function valid()
+                    {
+                        $echoStr = $_GET["echostr"];
+                
+                        //valid signature , option
+                        if($this->checkSignature()){
+                            echo $echoStr;
+                            exit;
+                        }
+     
+                    }
+                
+                        
+                    public function checkSignature()
+                    {
+                        // you must define TOKEN by yourself
+                        if (!defined("TOKEN")) {
+                            throw new Exception('TOKEN is not defined!');
+                        }
+                        
+                        $signature = $_GET["signature"];
+                        $timestamp = $_GET["timestamp"];
+                        $nonce = $_GET["nonce"];
+                                
+                        $token = TOKEN;
+                        $tmpArr = array($token, $timestamp, $nonce);
+                        // use SORT_STRING rule
+                        sort($tmpArr, SORT_STRING);
+                        $tmpStr = implode( $tmpArr );
+                        $tmpStr = sha1( $tmpStr );
+                        
+                        if( $tmpStr == $signature ){
+                            return true;
+                        }else{
+                            return false;
+                        }
+                    }
+1
+<br>
 }
 
-class wechatCallbackapiTest
-{
-    public function valid()
-    {
-        $echoStr = $_GET["echostr"];
-        if($this->checkSignature()){
-            echo $echoStr;
-            exit;
-        }
-    }
-
-    private function checkSignature()
-    {
-        $signature = $_GET["signature"];
-        $timestamp = $_GET["timestamp"];
-        $nonce = $_GET["nonce"];
-
-        $token = TOKEN;
-        $tmpArr = array($token, $timestamp, $nonce);
-        sort($tmpArr);
-        $tmpStr = implode( $tmpArr );
-        $tmpStr = sha1( $tmpStr );
-
-        if( $tmpStr == $signature ){
-            return true;
-        }else{
-            return false;
-        }
-    }
-
-    public function responseMsg()
-    {
-        $postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
-
-        if (!empty($postStr)){
-            $postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
-            $fromUsername = $postObj->FromUserName;
-            $toUsername = $postObj->ToUserName;
-            $keyword = trim($postObj->Content);
-            $time = time();
-            $textTpl = "<xml>
-                        <ToUserName><![CDATA[%s]]></ToUserName>
-                        <FromUserName><![CDATA[%s]]></FromUserName>
-                        <CreateTime>%s</CreateTime>
-                        <MsgType><![CDATA[%s]]></MsgType>
-                        <Content><![CDATA[%s]]></Content>
-                        <FuncFlag>0</FuncFlag>
-                        </xml>";
-            if($keyword == "?" || $keyword == "？")
-            {
-                $msgType = "text";
-                $contentStr = date("Y-m-d H:i:s",time());
-                $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
-                echo $resultStr;
-            }
-        }else{
-            echo "";
-            exit;
-        }
-    }
-}
-
-function traceHttp()
-{
-    logger("\n\nREMOTE_ADDR:".$_SERVER["REMOTE_ADDR"].(strstr($_SERVER["REMOTE_ADDR"],'101.226')? " FROM WeiXin": "Unknown IP"));
-    logger("QUERY_STRING:".$_SERVER["QUERY_STRING"]);
-}
-function logger($log_content)
-{
-    if(isset($_SERVER['HTTP_APPNAME'])){   //SAE
-        sae_set_display_errors(false);
-        sae_debug($log_content);
-        sae_set_display_errors(true);
-    }else{ //LOCAL
-        $max_size = 500000;
-        $log_filename = "log.xml";
-        if(file_exists($log_filename) and (abs(filesize($log_filename)) > $max_size)){unlink($log_filename);}
-        file_put_contents($log_filename, date('Y-m-d H:i:s').$log_content."\r\n", FILE_APPEND);
-    }
-}
 ?>
